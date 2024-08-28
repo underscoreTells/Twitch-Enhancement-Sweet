9e15cb7efbf28423e56a865be3654214303ec48d
Qlassy <alex.tellier01@gmail.com>
2024-08-28 08:47:06 -0400

    Started work on authentication logic. Re implemented Oauth service as composition and not aggregation

--- src/oauth2-auth-service.js
+++ src/oauth2-auth-service.js
@@ -2,32 +2,31 @@ import { AuthServiceInterface } from "./auth-service-interface";
 import { Logger } from "./logger";
 
 export class OAuth2AuthService extends AuthServiceInterface {
-    constructor({ clientId, clientSecret, tokenUrl, redirectUri, scope }) {
+    constructor({ clientId, clientSecret, tokenUrl, redirectUri }) {
         this.clientId = clientId;
         this.clientSecret = clientSecret;
         this.tokenUrl = tokenUrl;
         this.redirectUri = redirectUri;
-        this.scope = scope;
 
         this.accessToken = null;
         this.refreshToken = null;
         this.tokenExpiresIn = null;
     }
 
-    async getAccessToken() {
+    async getAccessToken(scope) {
         if (!this.accessToken || this.isTokenExpired()) {
             await this.refreshToken();
         }
         return this.accessToken;
     }
 
-    async refreshToken() {
+    async refreshToken(scope) {
         const params = new URLSearchParams();
         params.append('client_id', this.clientId);
         params.append('client_secret', this.clientSecret);
         params.append('grant_type', 'client_credentials');
         params.append('redirect_uri', this.redirectUri);
-        params.append('scope', this.scope);
+        params.append('scope', scope);
 
         try {
             const response = await fetch(this.tokenUrl, {
--- src/webhook-nightbot-handler.js
+++ src/webhook-nightbot-handler.js
@@ -1,18 +1,31 @@
 import { WebhookInterface } from 'webhook-handler-interface.js';
 import { SubscriptionService } from './subscription-service';
+import { OAuth2AuthService } from './oauth2-auth-service';
+import { nighbotClientID, nightbotClientSecret, nightbotTokenURL, nightbotRedirectURL } from '../utils/constants'
+import { Logger } from './logger';
 
 export class NightbotServiceHandler extends WebhookInterface {
-    constructor(expressApp, authService) {
+    constructor(expressApp) {
         this.server = expressApp;
-        this.authService = authService;
         this.subscriptionService = new SubscriptionService();
+
+        this.server.post('./nightbot-webhooks', this.#receive(req, res));
         
+        this.#newAuthService();
         this.#bind();
     }
     
     //TODO: Implement authetication logic with Nighbot
-    authenticate() {
-        
+    async authenticate() {
+        try {
+            this.commandToken = await this.authService.getAccessToken('commands');
+        }
+
+        catch (error) {
+            const logger = new Logger();
+
+            logger.log('authentication error for nightbot'); //TODO: Log error info
+        }
     }
 
     subscribe(event, subscriber) {
@@ -32,6 +45,16 @@ export class NightbotServiceHandler extends WebhookInterface {
 
     #notify(event) {
         this.subscriptionService.notify(event);
+    }
+
+    //Create new auth service - declutters constructor
+    #newAuthService() {
+        this.authService = new OAuth2AuthService({
+            clientId: nighbotClientID,
+            clientSecret: nightbotClientSecret,
+            tokenUrl: nightbotTokenURL,
+            redirectUri: nightbotRedirectURL,
+        });
     }
 
     //TODO: Bind methods to class in this body
